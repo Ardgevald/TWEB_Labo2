9d850fa2d87ade71c79d3e7ea82d1ad668368561
MiguelPomboDias <miguel.pombodias@heig-vd.ch>
2017-11-15 12:37:52 +0100

    bad comma

--- docs/js/getPokemon.js
+++ docs/js/getPokemon.js
@@ -3,11 +3,11 @@ function csvJSON(csv) {
   const lines = csv.split('\n');
   const result = [];
 
-  const headers = lines[0].split(',');
+  const headers = lines[0].split(';');
 
   for (let i = 1; i < lines.length; i += 1) {
     const obj = {};
-    const currentline = lines[i].split(',');
+    const currentline = lines[i].split(';');
 
     for (let j = 0; j < headers.length; j += 1) {
       obj[headers[j]] = currentline[j];
